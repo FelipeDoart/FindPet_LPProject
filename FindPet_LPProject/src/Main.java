14eb780892f67cc3b2e5b55f84a9d351c2c81570
FelipeDoart <flpd.araujo@outlook.com>
2025-11-17 21:25:03 -0300

    Versão: - Começo da main - Inicio do service,control; - Modelos admin e adotante adicionados.

--- FindPet_LPProject/src/Main.java
+++ FindPet_LPProject/src/Main.java
@@ -1,4 +1,96 @@
+
+import controller.AdminController;
+import controller.AdotantesController;
+import java.util.Scanner;;
 public class Main {
+
     public static void main(String[] args) {
-    }
+
+        Scanner sc = new Scanner(System.in);
+        AdotantesController controller = new AdotantesController();
+        AdminController adminController = new AdminController();
+
+        int opcao = 0;
+
+        while (opcao != 4) {
+            System.out.println("======================");
+            System.out.println("\nBem Vindo ao FindPet!");
+            System.out.println("======================");
+            System.out.println("1 - Quero adotar!");
+            System.out.println("2 - Quero ser voluntário!");
+            System.out.println("3 - Login Administrativo");
+            System.out.println("4 - Sair");
+            System.out.print("Escolha: ");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("\n--- Cadastro Adotantes ---");
+                    System.out.print("Nome: ");
+                    String nome1 = sc.nextLine();
+                    System.out.print("Email: ");
+                    String email1 = sc.nextLine();
+                    System.out.print("Telefone (opcional): ");
+                    String tel1 = sc.nextLine();
+                    controller.cadastrarPessoaComum(nome1, email1, tel1);
+                    break;
+                case 2:
+                    System.out.println("\n--- Cadastro Voluntário ---");
+                    System.out.print("Nome: ");
+                    String nome2 = sc.nextLine();
+                    System.out.print("Email: ");
+                    String email2 = sc.nextLine();
+                    System.out.print("Telefone (OBRIGATÓRIO): ");
+                    String tel2 = sc.nextLine();
+                    controller.cadastrarVoluntario(nome2, email2, tel2);
+                    break;
+
+                case 3:
+                    System.out.println("\n=== LOGIN ADMIN ===");
+                    System.out.print("Usuário: ");
+                    String usuario = sc.nextLine();
+
+                    System.out.print("Senha: ");
+                    String senha = sc.nextLine();
+
+                    if (adminController.loginAdmin(usuario, senha)) {
+                        System.out.println("\nLogin bem-sucedido!");
+
+                        int opcaoAdmin;
+                        do {
+                            adminController.mostrarMenuAdmin();
+                            System.out.print("Escolha: ");
+                            opcaoAdmin = sc.nextInt();
+                            sc.nextLine();
+
+                            switch (opcaoAdmin) {
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 0:
+                                    System.out.println("Saindo do menu admin...");
+                                    break;
+                            }
+                        } while (opcaoAdmin != 0);
+
+                    } else {
+                        System.out.println("Usuário ou senha incorretos.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Encerrando...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }sc.close();}
 }
+
